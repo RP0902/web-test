52bae82f7b3414539c3be971c497aa746b1eb087
RP0902 <yh.pei2009@gmail.com>
2025-06-19 14:53:26 +0800

    Keep header visible and centered

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-let lastScroll = 0;
-const header = document.querySelector('header');
-window.addEventListener('scroll', () => {
-  const current = window.pageYOffset || document.documentElement.scrollTop;
-  if (current > lastScroll) {
-    header.classList.add('hide');
-  } else {
-    header.classList.remove('hide');
-  }
-  lastScroll = current <= 0 ? 0 : current;
-});
+// Header scroll functionality removed; header remains visible.
